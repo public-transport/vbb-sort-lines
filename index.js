5149b12fbbd899f0e5cc14c3e88fd5d875830bc0
Jannis R <mail@jannisr.de>
2017-08-08 12:55:46 +0200

    ES6 -> ES5

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 'use strict'
 
-const parse = require('vbb-parse-line')
+var parse = require('vbb-parse-line')
 
-const modes = [
+var modes = [
 	'train',
 	'ferry',
 	'bus',
 	'walking'
 ]
 
-const sort = (lines) => lines.sort((a, b) => {
-	const d = modes.indexOf(a.mode) - modes.indexOf(b.mode)
+var compare = function (a, b) {
+	var d = modes.indexOf(a.mode) - modes.indexOf(b.mode)
 	if (d === 0) {
-		const _a = parse(a.name)
-		const _b = parse(b.name)
+		var _a = parse(a.name)
+		var _b = parse(b.name)
 
 		if (_a.symbol && !_b.symbol) return -1
 		if (_b.symbol && !_a.symbol) return 1
 		return _a.nr - _b.nr
-	} else return d
-})
+	}
+	return d
+}
 
+var sort = function (lines) {
+	return lines.sort(compare)
+}
+
+sort.compare = compare
 module.exports = sort
